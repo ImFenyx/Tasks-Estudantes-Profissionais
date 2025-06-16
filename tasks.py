6cbaf9483889009b37426af2c45ff0f7dce86534
ImFenyx <103691581+ImFenyx@users.noreply.github.com>
2025-06-16 13:01:53 -0300

    🎉 | begin project, pending task 1

---
+++ tasks.py
@@ -0,0 +1,11 @@
+while True:
+    print("Menu:")
+    pastel = 12
+    acai = 11
+    xburger = 15
+    
+    option = input("(1) Pastel\n(2) Açaí\n(3) X-Burger\n(q) Sair\n> ")
+    
+    if option == "q":
+        print("Saindo...")
+        break
